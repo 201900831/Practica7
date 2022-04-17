ccaf45d65315b983a47c2577cfe449054aeac661
lolar <lolarocamorlan@hotmail.com>
2022-04-17 19:05:45 +0200

    m

---
+++ baseDatos/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO WINE VALUES(1, "Rioja","2008","Muy rico");
+INSERT INTO WINE VALUES(2, "Valduero","2008","Muy rico");
+INSERT INTO WINE VALUES(3, "Rioja","2008","Muy rico");
+INSERT INTO WINE VALUES(4, "Valduero","2007","Muy rico");
+INSERT INTO WINE VALUES(5, "Rioja","2007","Muy rico");
+INSERT INTO WINE VALUES(6, "Rioja","2001","Muy rico");
+INSERT INTO WINE VALUES(7, "Rioja","2008","Muy rico");
